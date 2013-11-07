822c6d56e1ea5840b6cacb133680b5ddf815bb9e
Marius Küng <mk@mariuskueng.ch>
2013-11-07 15:34:41 +0100

    TestReport
    Not sure about what the submit method should actually do

--- src/paperless/TestReport.java
+++ src/paperless/TestReport.java
@@ -1,8 +1,25 @@
 package paperless;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestReport {
+	
+	List<MultipleChoiceQuestion> reportQuestions;
+	
+	public TestReport(){
+		this.reportQuestions= new ArrayList<MultipleChoiceQuestion>();
+	}
 
 	public void submit() {
+		for (int i = 0; i < this.reportQuestions.size(); i++) {
+			System.out.println(this.reportQuestions.get(i).getText()+" - "+this.reportQuestions.get(i).isCorrect());
+		}
+	}
 
+	public void addTestResult(MultipleChoiceQuestion q, boolean isResCorrect) {
+		q.setCorrect(isResCorrect);
+		this.reportQuestions.add(q);
 	}
 
 }
